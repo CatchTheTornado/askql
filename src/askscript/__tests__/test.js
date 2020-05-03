b2010ce40f8653a9eb7a1e897fed289f8791c1ec
Lukasz Czerwinski <czerwinskilukasz1@gmail.com>
2020-05-03 17:33:55 +0200

    AskScript: Simple tests improved

--- src/askscript/__tests__/test.js
+++ src/askscript/__tests__/test.js
@@ -1,17 +1,42 @@
 const fs = require('fs');
+const path = require('path')
 
 // Include AskScript parser
 const parser = require('../parser/askscript.grammar');
 
+const testFiles = {
+    'empty ask file': 'program01-empty.ask',
+    'variable':       'program02-variable.ask',
+    'string literal': 'program03-string.ask',
+
+}
+
 describe('AskScript parser', () => {
-    test('parses an empty ask file', () => {
 
-        const code = `ask {
-        }`;
-
+    function checkIfParses(code) {
         // AskScript -> AskJSX AST
         const askJsxStructure = parser.parse(code).print();
 
         expect(askJsxStructure).not.toBeNull();
-    });
+    }
+
+    function checkIfParsesFile(filename) {
+        const absoluteFilePath = path.join(__dirname, 'code', filename);
+        const code = fs.readFileSync(absoluteFilePath).toString();
+        checkIfParses(code);
+    }
+
+    const tests = [
+        'empty ask file',
+        'variable',
+        'string literal'
+    ];
+
+    for (const testName of tests) {
+        const testFilePath = testFiles[testName];
+        test(`parses ${testName}`, () => {
+            checkIfParsesFile(testFilePath);
+        });
+    }
+    
 });

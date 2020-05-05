c98924ee627d2ab29959b1625e01a20dd61e1f04
Lukasz Czerwinski <czerwinskilukasz1@gmail.com>
2020-05-05 13:21:08 +0200

    AskScript: Added new tests

--- src/askscript/__tests__/test.js
+++ src/askscript/__tests__/test.js
@@ -1,47 +1,27 @@
 const fs = require('fs');
-const path = require('path')
+const glob = require('glob');
+const path = require('path');
 
 // Include AskScript parser
 const parser = require('../parser/askscript.grammar');
 
-const testFilenames = {
-    'empty ask file': 'program01-empty.ask',
-    'variable':       'program02-variable.ask',
-    'string literal': 'program03-string.ask',
-    
-    // here will come 3 examples for graphql
-    
-    // here will come an example for call() with a lambda function
-}
 
-describe('AskScript parser', () => {
+describe('AskScript parser can parse', () => {
 
-    function checkIfParses(code) {
+    function checkIfParsesFile(absoluteFilePath) {
+        const code = fs.readFileSync(absoluteFilePath).toString();
+
         // AskScript -> AskJSX AST
         const askJsxStructure = parser.parse(code).print();
 
         expect(askJsxStructure).not.toBeNull();
     }
 
-    function checkIfParsesFile(filename) {
-        const absoluteFilePath = path.join(__dirname, 'code', filename);
-        const code = fs.readFileSync(absoluteFilePath).toString();
-        checkIfParses(code);
-    }
+    const testsGlobPath = path.join(__dirname, 'code', '*.ask');
+    const testFilenames = glob.sync(testsGlobPath);
 
-    const tests = [
-        'empty ask file',
-        'variable',
-        'string literal',
-        // here will come 3 examples for graphql
-
-        // here will come an example for call() with a lambda function
-    ];
-
-    for (const testName of tests) {
-        const testFilename = testFilenames[testName];
-
-        test(`parses ${testName}`, () => {
+    for (const testFilename of testFilenames) {
+        test(`parses ${testFilename}`, () => {
             checkIfParsesFile(testFilename);
         });
     }
@@ -49,39 +29,31 @@ describe('AskScript parser', () => {
 });
 
 
-describe('AskScript parser', () => {
+describe('AskScript parser output', () => {
     
-    function checkIfParsedFileMatchesOutput(testFilename, outputFilename) {
-        const absoluteFilePath = path.join(__dirname, 'code', testFilename);
+    function checkIfParsedFileMatchesOutput(absoluteFilePath, absoluteOutputFilePath) {
         const code = fs.readFileSync(absoluteFilePath).toString();
         
         // AskScript -> AskJSX AST
         const askJsxStructure = parser.parse(code).print();
         
-        const absoluteOutputFilePath = path.join(__dirname, 'output', outputFilename);
-        let tmp;
         // Using eval() instead of JSON.parse() saves us from writing a ton of quotes in object keys, which would degrade readability.
+        let tmp;
         const expectedOutput = eval('tmp = ' + fs.readFileSync(absoluteOutputFilePath).toString());
+        
         expect(askJsxStructure).toEqual(expectedOutput);
     }
 
 
-    const testsOut = [
-        'empty ask file',
-        'variable',
-        'string literal',
-        // here will come 3 examples for graphql
+    const outputGlobPath = path.join(__dirname, 'code', 'output', '*.ask.out');
+    const outputFilePaths = glob.sync(outputGlobPath);
 
-        // here will come an example for call() with a lambda function
-    ];
+    for (const outputFilePath of outputFilePaths) {
+        const parts = path.parse(outputFilePath);
+        const testFilePath = path.join(parts.dir, '..', parts.name);
 
-    for (const testName of testsOut) {
-        const testFilename = testFilenames[testName];
-
-        const parts = path.parse(testFilename);
-        const outputFilename = parts.name + '.out';
-        test(`parsed ${testName} matches ${outputFilename}`, () => {
-            checkIfParsedFileMatchesOutput(testFilename, outputFilename);
+        test(`parsed ${parts.name} matches ${parts.base}`, () => {
+            checkIfParsedFileMatchesOutput(testFilePath, outputFilePath);
         });
     }
     

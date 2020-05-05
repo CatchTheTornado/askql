ecb307facc5e78053116935d9cd393b50f33b5ea
Marcin Hagmajer <mhagmajer@gmail.com>
2020-05-05 22:29:46 +0200

    feat: e2e test

--- src/__tests__/script2vm.ts
+++ src/__tests__/script2vm.ts
@@ -5,7 +5,16 @@ import { parse } from '../askcode';
 import * as jsx from '../askjsx';
 import { run } from '../askvm';
 
-test('test stub', () => {
+function e2e(code: string): any {
+  const ast = script.parser.parse(code).print();
+  const program = jsx.load(ast);
+  const rendered = jsx.render(program);
+  const parsed = parse(rendered);
+  const result = run(parsed);
+  return result;
+}
+
+test('long', () => {
   const code = fs
     .readFileSync(
       path.join(__dirname, '../askscript/__tests__/code/program03-string.ask')
@@ -20,4 +29,8 @@ test('test stub', () => {
   expect(parsed).toHaveProperty('type');
   const result = run(parsed);
   expect(result).toBe('Hello world!');
+});
+
+test('e2e', () => {
+  expect(e2e('ask { "Hello world!"}')).toBe('"Hello world!"');
 });
--- src/askscript/__tests__/test.js
+++ src/askscript/__tests__/test.js
@@ -1,88 +1,88 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
 // Include AskScript parser
 const parser = require('../parser/askscript.grammar');
 
 const testFilenames = {
-    'empty ask file': 'program01-empty.ask',
-    'variable':       'program02-variable.ask',
-    'string literal': 'program03-string.ask',
-    
-    // here will come 3 examples for graphql
-    
-    // here will come an example for call() with a lambda function
-}
+  'empty ask file': 'program01-empty.ask',
+  variable: 'program02-variable.ask',
+  'string literal': 'program03-string.ask',
+
+  // here will come 3 examples for graphql
+
+  // here will come an example for call() with a lambda function
+};
 
 describe('AskScript parser', () => {
+  function checkIfParses(code) {
+    // AskScript -> AskJSX AST
+    const askJsxStructure = parser.parse(code).print();
 
-    function checkIfParses(code) {
-        // AskScript -> AskJSX AST
-        const askJsxStructure = parser.parse(code).print();
+    expect(askJsxStructure).not.toBeNull();
+  }
 
-        expect(askJsxStructure).not.toBeNull();
-    }
+  function checkIfParsesFile(filename) {
+    const absoluteFilePath = path.join(__dirname, 'code', filename);
+    const code = fs.readFileSync(absoluteFilePath).toString();
+    checkIfParses(code);
+  }
 
-    function checkIfParsesFile(filename) {
-        const absoluteFilePath = path.join(__dirname, 'code', filename);
-        const code = fs.readFileSync(absoluteFilePath).toString();
-        checkIfParses(code);
-    }
+  const tests = [
+    'empty ask file',
+    'variable',
+    'string literal',
+    // here will come 3 examples for graphql
 
-    const tests = [
-        'empty ask file',
-        'variable',
-        'string literal',
-        // here will come 3 examples for graphql
+    // here will come an example for call() with a lambda function
+  ];
 
-        // here will come an example for call() with a lambda function
-    ];
+  for (const testName of tests) {
+    const testFilename = testFilenames[testName];
 
-    for (const testName of tests) {
-        const testFilename = testFilenames[testName];
-
-        test(`parses ${testName}`, () => {
-            checkIfParsesFile(testFilename);
-        });
-    }
-    
+    test(`parses ${testName}`, () => {
+      checkIfParsesFile(testFilename);
+    });
+  }
 });
 
-
 describe('AskScript parser', () => {
-    
-    function checkIfParsedFileMatchesOutput(testFilename, outputFilename) {
-        const absoluteFilePath = path.join(__dirname, 'code', testFilename);
-        const code = fs.readFileSync(absoluteFilePath).toString();
-        
-        // AskScript -> AskJSX AST
-        const askJsxStructure = parser.parse(code).print();
-        
-        const absoluteOutputFilePath = path.join(__dirname, 'output', outputFilename);
-        let tmp;
-        // Using eval() instead of JSON.parse() saves us from writing a ton of quotes in object keys, which would degrade readability.
-        const expectedOutput = eval('tmp = ' + fs.readFileSync(absoluteOutputFilePath).toString());
-        expect(askJsxStructure).toEqual(expectedOutput);
-    }
+  function checkIfParsedFileMatchesOutput(testFilename, outputFilename) {
+    const absoluteFilePath = path.join(__dirname, 'code', testFilename);
+    const code = fs.readFileSync(absoluteFilePath).toString();
 
+    // AskScript -> AskJSX AST
+    const askJsxStructure = parser.parse(code).print();
 
-    const testsOut = [
-        'empty ask file',
-        'variable',
-        'string literal',
-        // here will come 3 examples for graphql
+    const absoluteOutputFilePath = path.join(
+      __dirname,
+      'output',
+      outputFilename
+    );
+    let tmp;
+    // Using eval() instead of JSON.parse() saves us from writing a ton of quotes in object keys, which would degrade readability.
+    const expectedOutput = eval(
+      'tmp = ' + fs.readFileSync(absoluteOutputFilePath).toString()
+    );
+    expect(askJsxStructure).toEqual(expectedOutput);
+  }
 
-        // here will come an example for call() with a lambda function
-    ];
+  const testsOut = [
+    'empty ask file',
+    'variable',
+    'string literal',
+    // here will come 3 examples for graphql
 
-    for (const testName of testsOut) {
-        const testFilename = testFilenames[testName];
+    // here will come an example for call() with a lambda function
+  ];
 
-        const parts = path.parse(testFilename);
-        const outputFilename = parts.name + '.out';
-        test(`parsed ${testName} matches ${outputFilename}`, () => {
-            checkIfParsedFileMatchesOutput(testFilename, outputFilename);
-        });
-    }
-    
+  for (const testName of testsOut) {
+    const testFilename = testFilenames[testName];
+
+    const parts = path.parse(testFilename);
+    const outputFilename = parts.name + '.out';
+    test(`parsed ${testName} matches ${outputFilename}`, () => {
+      checkIfParsedFileMatchesOutput(testFilename, outputFilename);
+    });
+  }
 });

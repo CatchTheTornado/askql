53fbbd706d9840682a4db3f62bd96938fffca43f
Marcin Hagmajer <mhagmajer@gmail.com>
2020-05-08 16:28:51 +0200

    feat: fix sum

--- src/askvm/resources/math/sum.ts
+++ src/askvm/resources/math/sum.ts
@@ -1,9 +1,14 @@
 import { resource } from '../../lib/resource';
 import { lambda, string, Typed, untyped } from '../../lib/typed';
+import { flatten } from '../../../utils';
 
 export const sum = resource<Typed<(a: number, b: number) => number>>({
   type: lambda(string, string),
-  resolver(a: number, b: number): number {
-    return untyped(a) + untyped(b);
+  resolver(...numbers: number[]): number {
+    const flatNumbers = flatten(untyped(numbers), {
+      arrays: true,
+      objectValues: true,
+    }) as number[];
+    return flatNumbers.reduce((a, x) => a + x, 0);
   },
 });

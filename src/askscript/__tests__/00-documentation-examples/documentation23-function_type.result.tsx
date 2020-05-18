e183a17a4cf861e0ea1c1d3b2bc0e2788753683b
Lukasz Czerwinski <czerwinskilukasz1@gmail.com>
2020-05-18 23:26:24 +0200

    AskScript: Added a dozen of tests for code snippets from our documentation

---
+++ src/askscript/__tests__/00-documentation-examples/documentation23-function_type.result.tsx
@@ -0,0 +1,18 @@
+export const expectedResult = {
+  "name": "fun",
+  "params": [
+    {
+      "name": "let",
+      "params": [
+        "n",
+        {
+          "name": "get",
+          "params": [
+            "$0"
+          ]
+        }
+      ]
+    },
+    ""
+  ]
+};

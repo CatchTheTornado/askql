eec66fa530ea165661700a367607e11652ff2bde
Marcin Hagmajer <mhagmajer@gmail.com>
2020-05-19 22:02:49 +0200

    fix(test): documentation10-function

--- src/askjsx/lib/Ask.tsx
+++ src/askjsx/lib/Ask.tsx
@@ -2,7 +2,12 @@ import { AskCodeOrValue } from '../../askcode';
 import * as askjsx from './jsx';
 askjsx;
 
-export function Ask({ children = [] }: { children?: AskCodeOrValue[] }) {
+export function Ask({
+  children = [],
+}: {
+  children?: AskCodeOrValue[];
+  returns?: string;
+}) {
   return (
     <call>
       <fun>{children}</fun>

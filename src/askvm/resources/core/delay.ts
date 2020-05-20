7f2e14dcac5b86b5504a26074e5c4f08d529b7aa
Piotr Zientara <314zientara@gmail.com>
2020-05-20 18:11:55 +0200

    fix(askscript):delay

--- src/askvm/resources/core/delay.ts
+++ src/askvm/resources/core/delay.ts
@@ -2,7 +2,8 @@ import { any, resource } from '../../lib';
 
 export const delay = resource({
   type: any,
-  async resolver() {
-    return new Promise((resolve) => setTimeout(resolve, 1000));
+  async resolver(input:any){
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return input;
   },
 });

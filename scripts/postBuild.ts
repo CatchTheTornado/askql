9bd141df4cf5f4d3419115459c049ba2370eea3b
986e8a573b989f39bbd6f78b7397ce74e00a06ee <kra.yonatan@gmail.com>
2020-06-07 19:15:44 +0300

    Use fs.promises as fs

--- scripts/postBuild.ts
+++ scripts/postBuild.ts
@@ -1,8 +1,8 @@
-import {promises} from 'fs';
+import { promises as fs } from 'fs';
 
 async function main() {
     try {
-        await promises.copyFile('README.md', 'dist/README.md');
+        await fs.copyFile('README.md', 'dist/README.md');
         console.log('README.md was copied to dist');
     } catch (e) {
         console.error(e);

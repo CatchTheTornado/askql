d0d577820c1ddc405afd019a974f9619eaa09185
km4 <m1leskm4@gmail.com>
2020-06-11 22:13:43 +0200

    Add greaterThanOrEqual operator

--- src/askvm/resources/math/index.ts
+++ src/askvm/resources/math/index.ts
@@ -3,6 +3,7 @@ import { minus } from './minus';
 import { sum } from './sum';
 import { times } from './times';
 import { lessThan } from './lessThan';
+import { greaterOrEq } from './greaterOrEq';
 
 Object.assign(exports, {
   '+': sum,
@@ -10,12 +11,14 @@ Object.assign(exports, {
   '*': times,
   '/': divideBy,
   '<': lessThan,
+  '>=': greaterOrEq,
 });
 
 export * from './ceil';
 export * from './floor';
 export * from './divideBy';
 export * from './lessThan';
+export * from './greaterOrEq';
 export * from './max';
 export * from './minus';
 export { sum as plus, sum } from './sum';

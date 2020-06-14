1e4fb93ffe6f1f0b30e9a8bb16d5744073b9bbf5
Yonatan Kra <kra.yonatan@gmail.com>
2020-06-14 11:16:12 +0300

    Implement the askMiddleware

---
+++ src/askExpressMiddleware/askExpressMiddleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction } from "express";
+import { runUntyped, resources } from "../askvm";
+import { parse } from '../askcode';
+import { Request, Response } from 'express';
+
+export function askExpressMiddleware(values: any, callNext = true, passError = false) {
+    return async function(request: Request, response: Response, next: NextFunction) {
+        try {
+            const askScriptCode = request.body.code;
+            const queryResponse = await runUntyped({ values, resources }, parse(askScriptCode));
+            console.log('Here #@$');
+            response.json(queryResponse);
+        } catch(err) {
+            if (passError) {
+                return next(err);
+            }
+            console.error(err);
+        }
+        
+        if (callNext) {
+            return next();
+        }
+
+        return;
+    }
+}

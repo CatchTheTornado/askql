8bf57fb827b38b90826dd71e08ae7aed40b34175
Karol Milewski <m1leskm4@gmail.com>
2020-06-15 17:54:28 +0200

    feat(operator): greaterThanOrEqual operator (#141)
    * Add greaterThanOrEqual operator
    
    * greaterThanOrEqual tests and rename operator
    
    * Export correct namespace

---
+++ src/askscript/__tests__/09-operators/operators-08-greater_than_or_equal.test.result.ts
@@ -0,0 +1,1 @@
+export const expectedResult = 'It works!';
--- src/askvm/resources/math/index.ts
+++ src/askvm/resources/math/index.ts
@@ -4,6 +4,7 @@ import { sum } from './sum';
 import { times } from './times';
 import { lessThan } from './lessThan';
 import { greaterThan } from './greaterThan';
+import { greaterThanOrEqual } from './greaterThanOrEqual';
 
 Object.assign(exports, {
   '+': sum,
@@ -12,6 +13,7 @@ Object.assign(exports, {
   '/': divideBy,
   '<': lessThan,
   '>': greaterThan,
+  '>=': greaterThanOrEqual,
 });
 
 export * from './ceil';
@@ -19,6 +21,7 @@ export * from './floor';
 export * from './divideBy';
 export * from './lessThan';
 export * from './greaterThan';
+export * from './greaterThanOrEqual';
 export * from './max';
 export * from './minus';
 export { sum as plus, sum } from './sum';

d39220f51356441ec63d30ceeef3aa422cc0c239
km4 <m1leskm4@gmail.com>
2020-06-17 00:06:34 +0200

    chore(build): add rules on linter and fixx flag

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,13 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    'no-unused-vars': 'off',
+    'no-empty-pattern': 'off',
+    'no-undef': 'off',
+    'no-useless-catch': 'off',
+    'no-useless-escape': 'off',
+    'valid-typeof': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
 };

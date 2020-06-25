7735c947a76d51ed0dd3c77bc65e71bf9abe3c60
986e8a573b989f39bbd6f78b7397ce74e00a06ee <kra.yonatan@gmail.com>
2020-06-25 18:42:16 +0300

    Generate the package json in the dist folder on post build
    Remove package.json from ts.config
    Delete src/package.json

--- scripts/postBuild.ts
+++ scripts/postBuild.ts
@@ -1,12 +1,48 @@
 import { promises as fs } from 'fs';
 
+import packageJson from '../package.json';
+
+const propertiesToPreserveFromPackageJSON = [
+  'name',
+  'version',
+  'description',
+  'main',
+  'bin',
+  'repository',
+  'license',
+  'bugs',
+  'homepage',
+  'dependencies',
+  'peerDependencies',
+];
+
 async function main() {
+  await copyReadme();
+  await copyPackageJson();
+}
+
+async function copyReadme() {
   try {
     await fs.copyFile('README.md', 'dist/README.md');
     console.log('README.md was copied to dist');
   } catch (e) {
     console.error(e);
   }
+}
+
+async function copyPackageJson() {
+  const releasePackageJson: any = {};
+  Object.keys(packageJson).forEach((key) => {
+    if (propertiesToPreserveFromPackageJSON.indexOf(key) > -1) {
+      // @ts-ignore
+      releasePackageJson[key] = packageJson[key];
+    }
+  });
+  await fs.writeFile(
+    'dist/package.json',
+    JSON.stringify(releasePackageJson, null, 2)
+  );
+  console.log('Package.json was written to dist');
 }
 
 main();

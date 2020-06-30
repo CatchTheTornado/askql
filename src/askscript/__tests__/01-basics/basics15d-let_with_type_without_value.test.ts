37e35e05aa0be6eed7bee53d1e72972afff4c14b
Marcin Hagmajer <mhagmajer@users.noreply.github.com>
2020-06-30 23:31:26 +0200

    fix(tests): remove false positives (#204)
    * fix(tests): remove false positives
    
    * delete tests
    
    * delete tests
    
    * req changes

--- src/askscript/__tests__/01-basics/basics15d-let_with_type_without_value.test.ts
+++ src/askscript/__tests__/01-basics/basics15d-let_with_type_without_value.test.ts
@@ -1,5 +1,5 @@
 import program from './basics15d-let_with_type_without_value.ask';
 
 test('sample', async () => {
-  await expect(program([])).resolves.toBeDefined();
+  await expect(program([])).resolves.toBe(null);
 });

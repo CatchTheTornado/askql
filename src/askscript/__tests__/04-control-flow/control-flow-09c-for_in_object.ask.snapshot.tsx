d1a361f310d456449515aed56c33e937e99bb5af
Lukasz Czerwinski <czerwinskilukasz1@gmail.com>
2020-06-30 17:46:09 +0200

    test(askvm): for..in

--- src/askscript/__tests__/04-control-flow/control-flow-09c-for_in_object.ask.snapshot.tsx
+++ src/askscript/__tests__/04-control-flow/control-flow-09c-for_in_object.ask.snapshot.tsx
@@ -1,15 +1,31 @@
 export = (
   <ask args={<list />}>
+    <const
+      name="itemPrices"
+      type={<ref name="any" />}
+      value={
+        <struct>
+          {"a"}
+          {10}
+          {"b"}
+          {40}
+          {"c"}
+          {32}
+          {"d"}
+          {99}
+        </struct>
+      }
+    />
     <let name="mySum" type={<ref name="float" />} value={0} />
     <forIn
-      key={<let name="index" type={<ref name="int" />} />}
+      key={<let name="index" type={<ref name="any" />} />}
       in={<ref name="itemPrices" />}
     >
       <assign
         name="mySum"
         value={
           <call
-            name="plus"
+            name="+"
             args={
               <list>
                 <ref name="mySum" />
@@ -24,6 +40,7 @@ export = (
                 />
               </list>
             }
+            isOperator={true}
           />
         }
       />

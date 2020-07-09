8af4eed49c04162b008c7f0839e015339672fd5c
Richard <ricmoran@ebay.com>
2020-07-09 16:50:10 +0200

    fix(askvm): adds url param to fetch

--- src/askvm/resources/node/fetch.ts
+++ src/askvm/resources/node/fetch.ts
@@ -1,12 +1,11 @@
 import { any, resource } from '../../lib';
 
-const fetchFun = (global as any).fetch;
+const globalFetch = (global as any).fetch;
 
 export const fetch = resource({
   type: any,
-  async resolver() {
-    const url = 'https://jsonplaceholder.typicode.com/posts/1';
-    const response = await fetchFun(url);
+  async resolver(url: string) {
+    const response = await globalFetch(url);
     const json = await response.json();
     return json;
   },

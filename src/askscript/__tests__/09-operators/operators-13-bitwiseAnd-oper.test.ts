8efc78e88e4a7b9db02d0e67e2f6ed583f7ad550
Akeni <waimondfung@gmail.com>
2020-07-29 00:01:04 -0700

    added end case test to operator

--- src/askscript/__tests__/09-operators/operators-13-bitwiseAnd-oper.test.ts
+++ src/askscript/__tests__/09-operators/operators-13-bitwiseAnd-oper.test.ts
@@ -9,4 +9,5 @@ test('bitwiseAnd_operator', async () => {
   expect(await e2e('0 & 0', environment)).toBe(0);
   expect(await e2e('-55 & -55', environment)).toBe(-55);
   expect(await e2e('-10 & 10', environment)).toBe(2);
+  expect(await e2e('2147483647 & 2147483647', environment)).toBe(2147483647);
 });

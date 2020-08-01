a0603774aca24f6766f059e912d4bf4ac61b128a
Richard <ricmoran@ebay.com>
2020-08-01 12:31:35 +0200

    Update toInt.test.ts

--- src/askvm/resources/math/__tests__/toInt.test.ts
+++ src/askvm/resources/math/__tests__/toInt.test.ts
@@ -2,77 +2,100 @@ import { runUntyped } from '../../..';
 import { parse } from '../../../../askcode';
 import * as core from '../../core';
 import { toInt } from '../toInt';
+import { resources } from '../../../../askvm';
+import { e2e } from '../../../../utils/tools';
 
 describe('math - toInt', () => {
+  let input: any;
   let result: any;
 
   it(`should return 2 when given 2.3`, async () => {
-    result = await ask(`toInt(2.3)`);
+    given(2.3);
+
+    await execution();
 
     expect(result).toBe(2);
   });
 
   it(`should return 2 when given '2.3'`, async () => {
-    result = await ask(`toInt('2.3')`);
+    given('2.3');
+
+    await execution();
 
     expect(result).toBe(2);
   });
 
   it(`should return 3 when given 2.5`, async () => {
-    result = await ask(`toInt(2.5)`);
+    given(2.5);
+
+    await execution();
 
     expect(result).toBe(3);
   });
 
   it(`should return 3 when given '2.5'`, async () => {
-    result = await ask(`toInt('2.5')`);
+    given('2.5');
+
+    await execution();
 
     expect(result).toBe(3);
   });
 
   it(`should return -2 when given -2.5`, async () => {
-    result = await ask(`toInt(-2.5)`);
+    given(-2.5);
+
+    await execution();
 
     expect(result).toBe(-2);
   });
 
   it(`should return -2 when given '-2.5'`, async () => {
-    result = await ask(`toInt('-2.5')`);
+    given('-2.5');
+
+    await execution();
 
     expect(result).toBe(-2);
   });
 
   it(`should return -3 when given -2.51`, async () => {
-    result = await ask(`toInt(-2.51)`);
+    given(-2.51);
+
+    await execution();
 
     expect(result).toBe(-3);
   });
 
   it(`should return -3 when given '-2.51'`, async () => {
-    result = await ask(`toInt('-2.51')`);
+    given('-2.51');
+
+    await execution();
 
     expect(result).toBe(-3);
   });
 
-  /*
   it(`should return 1 when given true`, async () => {
-    result = await ask(`toInt(true)`);
+    given(true);
+
+    await execution();
 
     expect(result).toBe(1);
   });
 
   it(`should return 0 when given false`, async () => {
-    result = await ask(`toInt(false)`);
+    given(false);
+
+    await execution();
 
     expect(result).toBe(0);
   });
 
   it(`should return 0 when given null`, async () => {
-    result = await ask(`toInt(null)`);
+    given(null);
+
+    await execution();
 
     expect(result).toBe(0);
   });
-   */
 
   it('should throw when given non-number string', async () => {
     await expect(ask(`toInt('4a')`)).rejects.toStrictEqual(
@@ -91,6 +114,16 @@ describe('math - toInt', () => {
       new Error(`toInt is unable to convert given value to Integer.`)
     );
   });
+
+  function given(value: any) {
+    input = value;
+  }
+
+  async function execution() {
+    const environment = { resources };
+
+    result = await e2e(`toInt(${input})`, environment);
+  }
 
   function ask(code: string) {
     return runUntyped(

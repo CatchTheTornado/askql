47ab61fa482b1d36e3de868e2645af842c21e3d8
CAROLINE PELLET <pellet@spaziodati.eu>
2020-09-10 13:36:10 +0200

    test: use while inside a function

---
+++ src/askscript/__tests__/04-control-flow/program11-while-break-in-function.ast.tsx
@@ -0,0 +1,61 @@
+export = (
+  <ask args={<list />}>
+    <const
+      name="exampleFunction"
+      type={<ref name="any" />}
+      value={
+        <fun args={<list />} returns={<ref name="int" />}>
+          <let name="n" type={<ref name="any" />} value={1} />
+          <while
+            condition={
+              <call
+                name="<"
+                args={
+                  <list>
+                    <ref name="n" />
+                    {10}
+                  </list>
+                }
+                isOperator={true}
+              />
+            }
+          >
+            <if
+              condition={
+                <call
+                  name="=="
+                  args={
+                    <list>
+                      <ref name="n" />
+                      {3}
+                    </list>
+                  }
+                  isOperator={true}
+                />
+              }
+            >
+              <break />
+            </if>
+            <assign
+              name="n"
+              value={
+                <call
+                  name="+"
+                  args={
+                    <list>
+                      <ref name="n" />
+                      {1}
+                    </list>
+                  }
+                  isOperator={true}
+                />
+              }
+            />
+          </while>
+          <return value={<ref name="n" />} />
+        </fun>
+      }
+    />
+    <call name="exampleFunction" args={<list />} />
+  </ask>
+);

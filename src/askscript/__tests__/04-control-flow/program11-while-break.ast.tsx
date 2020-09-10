9dc197d0fc9c408b7e3e0a0fc4dbf9f6fee4e63e
CAROLINE PELLET <pellet@spaziodati.eu>
2020-09-10 13:36:10 +0200

    test:  for of, for in, for, while loops

---
+++ src/askscript/__tests__/04-control-flow/program11-while-break.ast.tsx
@@ -0,0 +1,52 @@
+export = (
+  <ask args={<list />}>
+    <let name="n" type={<ref name="any" />} value={1} />
+    <while
+      condition={
+        <call
+          name="<"
+          args={
+            <list>
+              <ref name="n" />
+              {10}
+            </list>
+          }
+          isOperator={true}
+        />
+      }
+    >
+      <if
+        condition={
+          <call
+            name="=="
+            args={
+              <list>
+                <ref name="n" />
+                {3}
+              </list>
+            }
+            isOperator={true}
+          />
+        }
+      >
+        <break />
+      </if>
+      <assign
+        name="n"
+        value={
+          <call
+            name="+"
+            args={
+              <list>
+                <ref name="n" />
+                {1}
+              </list>
+            }
+            isOperator={true}
+          />
+        }
+      />
+    </while>
+    <ref name="n" />
+  </ask>
+);

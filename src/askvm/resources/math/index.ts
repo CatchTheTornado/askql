33cfd5be3808425aa8e108ded2b3c4350049de4f
jamesd35 <jamesd35@users.noreply.github.com>
2020-09-11 05:00:45 -0700

    feat(askvm): toFloat (#439)
    Co-authored-by: jamesd35 <james.ding@sjsu.edu>
    Co-authored-by: Marcin Hagmajer <mhagmajer@users.noreply.github.com>

--- src/askvm/resources/math/index.ts
+++ src/askvm/resources/math/index.ts
@@ -1,15 +1,15 @@
-import { divideBy } from './divideBy';
-import { minus } from './minus';
-import { sum } from './sum';
-import { times } from './times';
-import { lessThan } from './lessThan';
-import { lessThanOrEqual } from './lessThanOrEqual';
-import { greaterThan } from './greaterThan';
-import { greaterThanOrEqual } from './greaterThanOrEqual';
 import { equals } from '../core';
-import { mod } from './mod';
 import { bitwiseAnd } from './bitwiseAnd';
 import { bitwiseOr } from './bitwiseOr';
+import { divideBy } from './divideBy';
+import { greaterThan } from './greaterThan';
+import { greaterThanOrEqual } from './greaterThanOrEqual';
+import { lessThan } from './lessThan';
+import { lessThanOrEqual } from './lessThanOrEqual';
+import { minus } from './minus';
+import { mod } from './mod';
+import { sum } from './sum';
+import { times } from './times';
 
 Object.assign(exports, {
   '+': sum,
@@ -26,20 +26,22 @@ Object.assign(exports, {
   '|': bitwiseOr,
 });
 
+export * from './bitwiseAnd';
+export * from './bitwiseOr';
 export * from './ceil';
-export * from './floor';
+export * from './div';
 export * from './divideBy';
-export * from './lessThan';
-export * from './lessThanOrEqual';
+export * from './floor';
 export * from './greaterThan';
 export * from './greaterThanOrEqual';
+export * from './lessThan';
+export * from './lessThanOrEqual';
 export * from './max';
 export * from './min';
 export * from './minus';
+export * from './mod';
 export { sum as plus, sum } from './sum';
 export { times as multiply, times } from './times';
+export * from './toFloat';
 export * from './toInt';
-export * from './mod';
-export * from './bitwiseAnd';
-export * from './bitwiseOr';
-export * from './div';
+

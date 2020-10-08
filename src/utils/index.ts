9166b5a9f49e0b5d605d1ee6bedaf1310387053c
yonatankra <yonatan.kra@vonage.com>
2020-10-08 16:06:22 +0300

    Remove unneeded import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import { JSONable } from '../askvm/lib';
-
 export function assert(condition: boolean, message: string): asserts condition {
   if (!condition) {
     throw new Error(`Assertion error: ${message}`);

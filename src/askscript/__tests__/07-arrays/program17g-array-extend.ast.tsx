d4b5dbc2debbc33fdc09e778556a2df1b3eaf578
yonatankra <yonatan.kra@vonage.com>
2020-10-08 16:47:34 +0300

    arrayExtend tests

---
+++ src/askscript/__tests__/07-arrays/program17g-array-extend.ast.tsx
@@ -0,0 +1,23 @@
+export = (
+  <ask args={<list />}>
+    <call
+      name="arrayExtend"
+      args={
+        <list>
+          <list>
+            {1}
+            {2}
+            {3}
+            {4}
+          </list>
+          <list>
+            {5}
+            {6}
+            {7}
+            {8}
+          </list>
+        </list>
+      }
+    />
+  </ask>
+);
--- src/askvm/resources/list/index.ts
+++ src/askvm/resources/list/index.ts
@@ -2,3 +2,5 @@ export { find } from './find';
 export { hasKey } from './hasKey';
 export { length } from './length';
 export { set } from './set';
+export { objectExtend } from './objectExtend';
+export { arrayExtend } from './arrayExtend';

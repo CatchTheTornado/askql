994052c836f45da2602f43deae1ca375b6d8fa61
yonatankra <yonatan.kra@vonage.com>
2020-10-09 16:41:19 +0300

    Add a filter resource

---
+++ src/askvm/resources/list/filter.ts
@@ -0,0 +1,30 @@
+import { isAskCode } from '../../../askcode';
+import { any, resource, runUntyped } from '../../lib';
+import { asyncFilter } from '../../../utils';
+
+/**
+ * This implements mapping over list of data - similar to Array.prototype.map in JavaScript
+ */
+export const filter = resource<any, any[]>({
+  type: any,
+  async resolver(
+    list: any[],
+    predicate: (value: any, key?: number, list?: any[]) => Promise<boolean>
+  ): Promise<any> {
+    if (!Array.isArray(list)) {
+      throw new Error('Expecting an array in filter');
+    }
+    return asyncFilter(list, predicate);
+  },
+  async compute(options, { params }) {
+    const [listChild, predChild] = params!;
+    const list = await runUntyped(options, listChild);
+    const pred = await runUntyped(options, predChild);
+    if (!isAskCode(pred)) {
+      throw new Error(`Cannot call ${String(pred)}`);
+    }
+
+    const predicate = (...args: any[]) => runUntyped(options, pred, args);
+    return this.resolver!(list, predicate);
+  },
+});

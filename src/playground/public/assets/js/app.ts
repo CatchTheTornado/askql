aa794231356d4429a9306f142eac9c25260589dc
km4 <m1leskm4@gmail.com>
2020-10-12 20:14:25 +0200

    feat(#379): exit alert on playground

--- src/playground/public/assets/js/app.ts
+++ src/playground/public/assets/js/app.ts
@@ -107,3 +107,12 @@ function removeFadeInClass() {
   const resultElem = document.getElementById('result')!;
   resultElem.classList.remove('fadeIn');
 }
+function exitEditor(editor: AceAjax.Editor) {
+  console.log(editor);
+  window.addEventListener('beforeunload', function (e) {
+    // Cancel the event
+    e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+    // Chrome requires returnValue to be set
+    e.returnValue = '';
+  });
+}

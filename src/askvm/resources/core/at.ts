6a01f5bc80b620d8562546d11e0255846b6cc15b
Dastin.Sandura <dastin.sandura@gft.com>
2020-10-22 16:58:51 +0200

    mend

--- src/askvm/resources/core/at.ts
+++ src/askvm/resources/core/at.ts
@@ -3,6 +3,10 @@ import { any, resource, typed } from '../../lib';
 export const at = resource({
   type: any,
   async resolver(listOrObject: any, key: any): Promise<any> {
+    if (!(key in listOrObject))
+      throw Error(
+        'Requested property key was not defined so it is not possible to get its value.NO'
+      );
     return typed(listOrObject[key]);
   },
 });

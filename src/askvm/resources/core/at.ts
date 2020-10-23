7b6b6ff2a0044528978cf0e9ca0116ec1e931026
Dastin.Sandura <dastin.sandura@gft.com>
2020-10-23 10:24:45 +0200

    throw error only if accessing a non existing property of a non-null object

--- src/askvm/resources/core/at.ts
+++ src/askvm/resources/core/at.ts
@@ -3,7 +3,11 @@ import { any, resource, typed } from '../../lib';
 export const at = resource({
   type: any,
   async resolver(listOrObject: any, key: any): Promise<any> {
-    if (!(key in listOrObject))
+    if (
+      typeof listOrObject === 'object' &&
+      listOrObject !== null &&
+      !(key in listOrObject)
+    )
       throw Error(
         'Requested property key was not defined so it is not possible to get its value.'
       );

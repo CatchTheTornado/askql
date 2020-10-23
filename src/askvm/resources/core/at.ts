762180e3984fee255906c63b9819205101655cb6
Dastin.Sandura <dastin.sandura@gft.com>
2020-10-23 10:16:12 +0200

    change message in the thrown error to match the unit test

--- src/askvm/resources/core/at.ts
+++ src/askvm/resources/core/at.ts
@@ -5,7 +5,7 @@ export const at = resource({
   async resolver(listOrObject: any, key: any): Promise<any> {
     if (!(key in listOrObject))
       throw Error(
-        'Requested property key was not defined so it is not possible to get its value.NO'
+        'Requested property key was not defined so it is not possible to get its value.'
       );
     return typed(listOrObject[key]);
   },

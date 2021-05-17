0831a4245a89bf3d7376b57c24ee603fa02d6f6c
Piotr Karwatka <piotr.karwatka@gmail.com>
2021-05-17 13:34:21 +0200

    [feat] additional tests

---
+++ src/askscript/__tests__/15-more-resources/_environment.ts
@@ -0,0 +1,14 @@
+import { Values } from '../../../askvm';
+export const values: Values = {
+  clientNames: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+  revPerClient: {
+    A: 136,
+    B: 426,
+    C: 133,
+    D: 35,
+    E: 246,
+    F: 446,
+    G: 53,
+  },
+  value: 5,
+};
